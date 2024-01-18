db32a4a966f71434dfe00eaa865adae61450a5ed
Mr-Aman26 <99275306+Mr-Aman26@users.noreply.github.com>
2024-01-18 14:40:23 +0530

    first addition

---
+++ db.js
@@ -0,0 +1,14 @@
+// const Pool = require("pg").Pool;
+
+import pg from "pg";
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+});
+
+pool.connect((err) => {
+  if (err) throw err;
+  console.log("Connect to PostgreSQL successfully!");
+});
+module.exports = pool;
---
+++ index.js
@@ -0,0 +1,87 @@
+// const express = require("express");
+// const app = express();
+// const port = 4000;
+// const pg = require("pg");
+// const pool = require("./db");
+
+// require("dotenv").config();
+
+// // PostgreSQL connection pool
+// // const pool = new pg.Pool({
+// //   connectionString: process.env.DATABASE_URL,
+// //   // connectionString: "postgres://postgres@localhost/bookstore"
+// // });
+
+// // const pool = new pg.Pool({
+// //   user: "postgres",
+// //   password: "Aman@123",
+// //   host: "localhost",
+// //   port: 5432,
+// //   database: "bookstore",
+// // });
+// // app.post("/todos", async (req, res) => {
+// //   try {
+// //     const { description } = req.body;
+// //     const newTodo = await pool.query(
+// //       "INSERT INTO todo (description) VALUES($1) RETURNING *",
+// //       [description]
+// //     );
+
+// //     res.json(newTodo.rows[0]);
+// //   } catch (err) {
+// //     console.error(err.message);
+// //   }
+// // });
+// // Error handling middleware
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send("Something went wrong!");
+// });
+
+// // Load routes
+// // const usersRoutes = require('./routes/users');
+
+// // Mount routes
+// // app.use('/users', usersRoutes);
+
+// // Start the server
+// app.listen(port, async () => {
+//   console.log(`Server listening on port ${port}`);
+
+//   try {
+//     const client = await pool.connect();
+
+//     // Create the table if it doesn't exist
+//     await client.query(
+//       "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT, email TEXT)"
+//     );
+
+//     // Insert a dummy entry
+//     await client.query("INSERT INTO users (name, email) VALUES ($1, $2)", [
+//       "John Doe",
+//       "johndoe@example.com",
+//     ]);
+
+//     client.release();
+//     console.log(
+//       "Users table created (if needed) and dummy user added successfully!"
+//     );
+//   } catch (error) {
+//     console.error("Error creating users table or dummy user:", error);
+//   }
+// });
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+app.use(express.json());
+
+const bookRouter = require("./routes/bookRouter");
+
+app.use("/api/v1/books", bookRouter);
+
+app.listen(process.env.PORT, () =>
+  console.log("Server is running on port 5000")
+);
